113e7a387005c932f032eee0aa7ddb29f41d5b25
Sampo Sahla <sahlaster95@gmail.com>
2022-11-14 17:47:29 +0200

    kitty transparency decrease, add floating clients

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -805,7 +805,8 @@ awful.rules.rules = {
             "gnome-calculator",
             "Solaar",
             "1Password",
-            "Nvidia-settings"
+            "Nvidia-settings",
+            "Postman"
         },
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
